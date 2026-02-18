7d9c35956d1e7a0b7de7979e2211e48a02423dc4
HoeumSenghun <hoeumsenghun369@gmail.com>
2026-02-18 08:23:14 +0700

    config tailwindcss

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-
-
 export default function Home() {
   return (
     <div>
-      <main >
-        hi
+      <main>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
   );
